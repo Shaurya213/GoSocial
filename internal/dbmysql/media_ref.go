7a4a59708b9efad6aeecd0a37b5ac21dec3dcb92
Jai Khurana <64320051+jaikhuranna@users.noreply.github.com>
2025-08-08 11:22:48 +0530

    add chats microservice and mongo powered http server (#4)
    * add go.mod and main.go
    
    * add go.mod
    
    * add chat.proto and pb files for chat, conversation and message for
    dbmysql package
    
    * add mongoDBConfig and mongoDB basic connections and db structs
    
    * add gitignore and media_ref.go
    
    * add over the basic mongo setup
    
    * working: mongodb upload and download
    
    * gorm struct and migrations define
    
    # Conflicts:
    #	internal/dbmysql/media_ref.go
    #	internal/dbmysql/message.go
    #	main.go
    
    * add gorm structs and migration files
    
    * add http server on local host; add cmd microservice for media-server
    
    * initial setup
    
    * add media_ref_repo with crate function which populates URL for each file
    as well
    
    * get docker-compose to work on localhost back again
    
    * add chat service(unary)
    
    * add chat handler
    
    * update handler and wire
    
    * add chat microservice main.go
    
    * add docker-compose-mongo and docker-compose-sql
    
    * add working chat microservice
    
    * update chat svc main
    
    * add machine ip getter in config
    
    * remove localhost temp testing
    
    * remove Dockerfile and media server container from compose-mongo
    
    * add localhost testing comment
    
    * add sql to docker-compose-mongo
    
    * update docker-compose-mongo
    
    * fix
    
    * add comments
    
    * update some files and add chat_repository_test
    
    ---------
    
    Co-authored-by: vikasch123 <kvikaskumar004@gmail.com>

---
+++ internal/dbmysql/media_ref.go
@@ -0,0 +1,23 @@
+package dbmysql
+
+import (
+	//"gosoca"
+	"gosocial/internal/common"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type MediaRef struct {
+	MediaRefID  uint                 `gorm:"column:media_ref_id;primaryKey" json:"id"`
+	FileID      string               `gorm:"size:24;uniqueIndex" json:"file_id"` // MongoDB ObjectID
+	Type        string               `gorm:"size:20" json:"type"`                // image, video, document
+	FileName    string               `gorm:"size:255" json:"file_name"`
+	ContentType common.MediaFileType `gorm:"size:100" json:"content_type"`
+	URL         string               `gorm:"size:500" json:"url"` // Auto-generated GridFS URL
+	Size        int64                `json:"size"`
+	UploadedBy  string               `gorm:"size:36;index" json:"uploaded_by"` // User ID
+	UploadedAt  time.Time            `gorm:"autoCreateTime " json:"uploaded_at"`
+	gorm.Model                       // Adds ID, CreatedAt, UpdatedAt, DeletedAt
+	//User        User	`gorm:""`
+}
---
+++ internal/di/wire.go
@@ -0,0 +1,37 @@
+//go:build wireinject
+// +build wireinject
+
+package di
+
+import (
+	"gosocial/internal/chat/handler"
+	"gosocial/internal/chat/repository"
+	"gosocial/internal/chat/service"
+	"gosocial/internal/dbmysql"
+	"gosocial/internal/config"
+
+	"github.com/google/wire"
+)
+
+// ChatProviderSet contains all providers for chat service
+var ChatProviderSet = wire.NewSet(
+	config.LoadConfig,
+	// Use existing database connection function
+	dbmysql.NewMySQL,
+	
+	// Repository layer
+	repository.NewChatRepository,
+	
+	// Service layer
+	service.NewChatService,
+	
+	// Handler layer
+	handler.NewChatHandler,
+)
+
+// InitializeChatService wires up all dependencies for the chat service
+func InitializeChatService() (*handler.ChatHandler, func(), error) {
+	wire.Build(ChatProviderSet)
+	return nil, nil, nil
+}
+
